cb91d5bb105848bc8d1936e6f81e764ee52e8d3a
Indra <indrasinghatcoding@gmail.com>
2023-03-04 13:27:14 +0530

    first route complete

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 console.log('My New Project');
+
+const express = require('express');
+
+const fs = require('fs');
+
+const app = express();
+
+const port = 4000;
+
+app.get('/send/:data', (req, res) => {
+    const data = req.params.data;
+
+    fs.writeFileSync('test.txt', data);
+    res.json({
+        message: 'Your data is saved',
+    })
+})
+
+app.listen(port, (err) => {
+    console.log(`App is listening at port ${port}`);
+})
